481cc9b9b03ad68dedc92956b2df91e6c623ecd2
Kristian Padilla <ketpadilla05@gmail.com>
2023-08-03 20:25:54 +0800

    finish comments

--- projects/multiFunctionCalculator.py
+++ projects/multiFunctionCalculator.py
@@ -1,63 +1,83 @@
 # LIBRARIES
 from sympy import sqrt, symbols, Eq, sympify, pretty
 from sympy.solvers import solve
-from re import sub, findall
+from re import sub
+
 
 # * OPERATIONS
 # ADDITION
 def add():
+    # Get numbers from user and convert them to integers
     inputs = [int(i) for i in input("Enter numbers to add, separate each number with a space: ").split()]
+    # Add the numbers
     return print(f"The sum of {' + '.join(map(str, inputs))} is {sum(inputs)}")
 
 
 # SUBTRACTION
 def subtract():
+    # Get numbers from user and convert them to integers
     inputs = [int(i) for i in input("Enter numbers to subtract, separate each number with a space: ").split()]
+    # Subtract the first number by the rest of the numbers
     return print(f"The difference of {' - '.join(map(str, inputs))} is {inputs[0] - sum(inputs[1:])}")
 
 
 # MULTIPLICATION
 def multiply():
+    # Get numbers from user
     inputs = [int(i) for i in input("Enter numbers to multiply, separate each number with a space: ").split()]
+    # Set the product to 1
     product = 1
+    # Multiply the product by each number
     for number in inputs:
         product *= number
+    # Print the product
     return print(f"The product of {' * '.join(map(str, inputs))} is {product}")
 
 
 # DIVISION
 def divide():
+    # Get numbers from user
     inputs = [int(i) for i in input("Enter numbers to divide, separate each number with a space: ").split()]
+    # Set the quotient to the first number
     quotient = inputs[0]
+    # Divide the quotient by the rest of the numbers
     for number in inputs[1:]:
         quotient /= number
+    # Print the quotient
     return print(f"The quotient of {' / '.join(map(str, inputs))} is {round(quotient, 2)}")
 
 
 # DETECT PRIME NUMBERS
 def detect_prime():
+    # Get number from user
     number = int(input("Enter a number to check if it is prime: "))
-    # Check if there exists any factor other than 1 and the number itself
+    # Check if number is prime or composite
     numberType = "prime" if not any(number % num == 0 for num in range(2, number)) else "composite"
+    # Print result
     return print(f"{number} is a {numberType} number.")
 
 
 # GENERATE PRIME FACTORS
 def generate_prime_factors():
+    # Get number from user
     number = int(input("Enter a number to generate its prime factors: "))
+    # Generate prime factors
     primeFactors = [num for num in range(1, number+1) if number % num == 0]
+    # Print factors
     return print(f"The prime factors of {number} are {primeFactors}")
 
 
 # SIMPLIFY SQUARE ROOTS
 def generate_square_roots():
+    # Get number from user
     number = int(input("Enter a number to simplify its square root: "))
+    # Perform square root and print in pretty format
     return print(f"The square root of {number} is {pretty(sqrt(number))}")
 
 
 # SOLVE EQUATIONS
 def solve_equations():
-    # Get the equation from the user
+    # Get the equation from user
     equation = problem = input("Enter an equation to solve: ")
     # Check if the equation is valid (contains only one equal sign)
     if "=" not in equation or equation.count("=") > 1:
@@ -119,5 +139,3 @@ def main():
 
 # * RUN PROGRAM
 main()
-
-
