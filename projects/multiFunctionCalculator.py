71e8097ab0c0cd6065fb64317695732bad12a48e
Kristian Padilla <ketpadilla05@gmail.com>
2023-08-03 20:08:48 +0800

    add functions

--- projects/multiFunctionCalculator.py
+++ projects/multiFunctionCalculator.py
@@ -1,17 +1,22 @@
 # LIBRARIES
+from sympy import sqrt, symbols, Eq, sympify, pretty
+from sympy.solvers import solve
+from re import sub, findall
 
-
-# OPERATIONS
+# * OPERATIONS
+# ADDITION
 def add():
     inputs = [int(i) for i in input("Enter numbers to add, separate each number with a space: ").split()]
     return print(f"The sum of {' + '.join(map(str, inputs))} is {sum(inputs)}")
 
 
+# SUBTRACTION
 def subtract():
     inputs = [int(i) for i in input("Enter numbers to subtract, separate each number with a space: ").split()]
     return print(f"The difference of {' - '.join(map(str, inputs))} is {inputs[0] - sum(inputs[1:])}")
 
 
+# MULTIPLICATION
 def multiply():
     inputs = [int(i) for i in input("Enter numbers to multiply, separate each number with a space: ").split()]
     product = 1
@@ -20,19 +25,71 @@ def multiply():
     return print(f"The product of {' * '.join(map(str, inputs))} is {product}")
 
 
+# DIVISION
 def divide():
     inputs = [int(i) for i in input("Enter numbers to divide, separate each number with a space: ").split()]
     quotient = inputs[0]
     for number in inputs[1:]:
         quotient /= number
-    return print(f"The quotient of {' / '.join(map(str, inputs))} is {quotient}")
+    return print(f"The quotient of {' / '.join(map(str, inputs))} is {round(quotient, 2)}")
 
 
-# TODO: add other operations
+# DETECT PRIME NUMBERS
+def detect_prime():
+    number = int(input("Enter a number to check if it is prime: "))
+    # Check if there exists any factor other than 1 and the number itself
+    numberType = "prime" if not any(number % num == 0 for num in range(2, number)) else "composite"
+    return print(f"{number} is a {numberType} number.")
+
+
+# GENERATE PRIME FACTORS
+def generate_prime_factors():
+    number = int(input("Enter a number to generate its prime factors: "))
+    primeFactors = [num for num in range(1, number+1) if number % num == 0]
+    return print(f"The prime factors of {number} are {primeFactors}")
+
+
+# SIMPLIFY SQUARE ROOTS
+def generate_square_roots():
+    number = int(input("Enter a number to simplify its square root: "))
+    return print(f"The square root of {number} is {pretty(sqrt(number))}")
+
+
+# SOLVE EQUATIONS
+def solve_equations():
+    # Get the equation from the user
+    equation = problem = input("Enter an equation to solve: ")
+    # Check if the equation is valid (contains only one equal sign)
+    if "=" not in equation or equation.count("=") > 1:
+        return print("Invalid equation. Please try again.")
+    # Extract variables from the equation
+    variables = {symbols(variable) for variable in set(equation) if variable.isalpha()}
+    # Format the equation
+    equation = sub(r'(\d)([a-zA-Z])', r'\1*\2', equation).replace(" ", "").replace("^", "**").split("=")
+    # If there are multiple variables, ask the user which one to solve for
+    if len(variables) > 1:
+        variable = input(f"Variables: {', '.join(map(str, variables))}\nEnter a variable to solve for: ")
+        if variable not in map(str, variables):
+            return print("Invalid variable.")
+    else:
+        # If there's only one variable, automatically select it
+        variable = next(iter(variables))
+    # Solve the equation
+    answer = solve(Eq(sympify(equation[0]), sympify(equation[1])), variable)    
+    # Check if there are any solutions
+    if len(answer) >= 1:
+        # Print the answer(s)
+        print(f"The solution{'s' if len(answer) > 1 else ''} to {problem} {'are:' if len(answer) > 1 else 'is:'}")
+        for ans in answer:
+            # Iterate through each answer
+            print(f" {variable} = {pretty(ans)}")
+        return
+    # If no solutions are found, print an error message
+    return print("No solutions found.")
 
 
 # INITIALIZE GLOBAL VARIABLES
-options = {'a': add,'b': subtract,'c': multiply,'d': divide}
+options = {'a': add,'b': subtract,'c': multiply,'d': divide, 'e': detect_prime, 'f': generate_prime_factors, 'g': generate_square_roots, 'h': solve_equations}
 
 
 # MENU SELECTION
@@ -41,9 +98,9 @@ def menu():
     while True:
         # Print the options
         for key, value in options.items():
-            print(f"{key}. {value.__name__}")
+            print(f"{key}. {value.__name__.replace('_',' ')}")
         # Get the user's choice
-        selectedOption = input("\n Select an option (only enter the letter): ")
+        selectedOption = input("\nSelect an option (only enter the letter): ")
         # Validate the user's choice
         if selectedOption.replace('.','').lower() in options:
             selectedOption = selectedOption.replace('.','').lower() # ! ensure option is lowercase and without a period
