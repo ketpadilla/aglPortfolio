889dd629a72563108f681fbf9e1ca330f7537f45
Kristian Padilla <ketpadilla05@gmail.com>
2023-08-06 18:01:43 +0800

    continue projectileGame

--- projects/projectileGame.py
+++ projects/projectileGame.py
@@ -1,11 +1,19 @@
 # LIBRARIES
 import matplotlib.pyplot as plt
 from random import randint
+from numpy import linspace, sin, cos, radians
 
-# INITIALIZE GLOBAL VARIABLES
+# INITIALIZE VARIABLES
+# Initialize score and launch angle
 score = 0
-xmin = ymin = -10
-xmax = ymax = 10
+launchAngle = randint(0, 90)
+# Initialize wall dimensions
+distance = randint(5, 15)
+wallDimensions = {'x': [distance, distance], 'y': [0, randint(2, 20)]}
+# Initialize graph dimensions
+xmin, ymin, xmax, ymax = -1, -1, wallDimensions['x'][0] + 3, wallDimensions['y'][1] + 3
+# Initialize font size
+fontSize = max(9, 2 * min((xmax - xmin) / distance, (ymax - ymin) / distance))
 
 
 # INITIALIE GRAPH
@@ -13,20 +21,64 @@ fig, ax = plt.subplots()
 
 
 # FUNCTIONS
+# Create rocket trajectory
+def trajectory(launchAngle, height, distance):
+    # Constants
+    g = 9.81  # Acceleration due to gravity (m/s^2)
+
+    # Convert launch angle from degrees to radians
+    theta = radians(launchAngle)
+
+    # Calculate time of flight
+    time_of_flight = (2 * height) / (g * sin(theta))
+
+    # Create time array from 0 to time_of_flight
+    t = linspace(0, time_of_flight, num=100)
+
+    # Calculate x and y positions of the rocket at each time step
+    x = distance * cos(theta) * t
+    y = height + (distance * sin(theta) * t) - (0.5 * g * t ** 2)
+
+    return x, y
+
+
 # Create graph
-def graph(x, y):
-    # Set axis lines to blue
-    plt.plot([xmin,xmax],[0,0],'b')
-    plt.plot([0,0],[ymin,ymax],'b')
-    # Plot point
-    plt.plot(x,y,'ro')
-    # Show grid
-    plt.grid()
+def graph(xmin, ymin, xmax, ymax, wallDimensions, launchAngle):
+    # Set axis lines to dashed black
+    plt.plot([xmin, xmax], [0, 0], 'k--')
+    plt.plot([0, 0], [ymin, ymax], 'k--')
+    # Plot rocket starting position
+    plt.plot(0,0,'ro')
+    # Plot wall
+    plt.plot(wallDimensions['x'], wallDimensions['y'], color='midnightblue', linewidth=5)
+
+
+
+    # TODO: FIX THIS
+    # Generate random height for the rocket
+    height = randint(5, 20)
+    # Calculate trajectory
+    x, y = trajectory(launchAngle, height, distance)
+    # Plot trajectory
+    plt.plot(x, y, label=f"Launch Angle: {launchAngle}°, Height: {height}")
+
+
+
+    # Show grid with 0.5 spacing
+    plt.xticks(range(xmin, xmax+1), fontsize=fontSize)
+    plt.yticks(range(ymin, ymax+1), fontsize=fontSize)
+    plt.grid(which='both', ls='--', lw=0.5, color='gray', alpha=0.5)
     # Set title
-    plt.title("Projectile Game")
+    plt.title("Projectile Game", fontsize=fontSize)
+    # Show coordinates of rocket and wall
+    plt.text(0.3, 0.3, f"Rocket: (0, 0)", fontsize=fontSize)
+    plt.text(wallDimensions['x'][0] + 0.3, wallDimensions['y'][1] + 0.3, f"Wall: ({wallDimensions['x'][0]}, {wallDimensions['y'][1]})", fontsize=fontSize)
+    # Add legend
+    plt.legend(loc='upper right', fontsize=fontSize)
     # Show graph
     plt.show()
     return
+
 
 
 # Check if ... 
@@ -41,13 +93,9 @@ def check():
 
 # * MAIN FUNCTION
 def main():
-    # Initialize global variables
-    global xmin, ymin, xmax, ymax
-    # Set starting point
-    x = y = 0
     # TODO: randomize function for height and wall location
     # Create graph
-    graph(x, y)
+    graph(xmin, ymin, xmax, ymax, wallDimensions, launchAngle)
     return
 
 
