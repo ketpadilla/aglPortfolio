7bcfcd2bfa4116272223f900329c6c606b7d746c
Kristian Padilla <ketpadilla05@gmail.com>
2023-08-11 14:30:12 +0800

    add parentGraphs

---
+++ topics/11_parentGraphs.py
@@ -0,0 +1,46 @@
+## PARENT GRAPHS - graph of a relatively simple function
+
+# --- #
+
+# * EXAMPLES
+# Horizontal line
+y = 2 
+
+# Linear
+y = x
+
+# Quadratic
+y = x**2
+
+# Cubic
+y = x**3
+
+# Quartic
+y = x**4
+
+# Quintic
+y = x**5
+
+# Absolute value
+y = abs(x)
+
+# Square root
+from numpy import sqrt, cbrt, floor
+y = sqrt(x)
+
+# Square root with rational exponent
+y = x**(1/2)
+
+# Cube root
+y = cbrt(x)
+
+# Floor
+y = floor(x)
+
+# Exponential
+y = 2**x
+
+# ! REFER FOR GRAPHS: https://colab.research.google.com/drive/1uwKuaHCC2WCUFKmXW-5NqWUlEP9ak7Pz?usp=sharing
+
+# --- #
+
