11913dac8fd1b89d944a99a8a67d45c4072fed73
Kristian Padilla <ketpadilla05@gmail.com>
2023-10-02 22:15:11 +0800

    start get file function

--- projects/dataGraphExplorer.py
+++ projects/dataGraphExplorer.py
@@ -22,13 +22,32 @@ def print_headings():
 # * HELPER FUNCTIONS
 # GET CVS FILE
 def get_file():
-    return file
+    fileOptions = ['upload', 'url', 'default']
+    # Ask user how they want to get the file
+    while True:
+        print("How would you like to get the file?")
+        for i, option in enumerate(fileOptions):
+            print(f"{i+1}. {option}")
+        userChoice = input("\nSelect an option (only enter the number): ")
+        # Validate user's choice
+        if userChoice.isnumeric() and int(userChoice) in range(1, len(fileOptions)+1):
+            userChoice = int(userChoice)
+            break
+        print(f"\n {userChoice} is not a valid option. Please try again.") # print error message if invalid and try again
+    # Get the file
+    if userChoice == 1: # upload
+        print("upload") #TODO
+    elif userChoice == 2: # url
+        print("url") #TODO
+    elif userChoice == 3: # default
+        print("default") #TODO
+    return
 
 
 # * FUNCTIONS
 # INITIALIZE VARIABLES
 options = {'a': print_headings}
-file = None
+file = []
 
 
 # MENU SELECTION
@@ -36,10 +55,11 @@ def menu():
     global options, graphDimensions, fontSize
     while True:
         # Print the options
+        print("\nWhat do you want to do with the file?")
         for key, value in options.items():
             print(f"{key}. {value.__name__.replace('_',' ')}")
         # Get the user's choice
-        selectedOption = input("\nSelect an option (only enter the letter): ")
+        selectedOption = input("\nOnly enter the letter: ")
         # Validate the user's choice
         if selectedOption.replace('.','').lower() in options:
             selectedOption = selectedOption.replace('.','').lower() # ! ensure option is lowercase and without a period
@@ -49,6 +69,7 @@ def menu():
 
 # * MAIN FUNCTION
 def main():
+    global file
     # Get .cvs file
     if not file: # if empty
         file = get_file() # get file
